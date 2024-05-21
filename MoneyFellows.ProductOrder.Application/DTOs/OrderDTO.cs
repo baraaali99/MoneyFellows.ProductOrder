5a3b04ebd3e56a499186b472349e94ad0013af6d
baraaali99 <baraa.ali1912@gmail.com>
2024-05-21 13:07:24 +0300

    - modified OrderDetailsDTO and OrderDTO

--- MoneyFellows.ProductOrder.Application/DTOs/OrderDTO.cs
+++ MoneyFellows.ProductOrder.Application/DTOs/OrderDTO.cs
@@ -5,7 +5,7 @@ public class OrderDTO
     public Guid Id { get; set; }
     public string DeliveryAddress { get; set; }
     public decimal TotalCost { get; set; }
-    public List<OrderDetailDTO> OrderDetails { get; set; }
+    public List<OrderDetailsDTO> OrderDetails { get; set; }
     public string CustomerDetails { get; set; }
     public DateTime DeliveryTime { get; set; }
 }
---
+++ MoneyFellows.ProductOrder.Application/DTOs/OrderDetailsDTO.cs
@@ -0,0 +1,11 @@
+namespace MoneyFellows.ProductOrder.Application.DTOs;
+
+public class OrderDetailsDTO
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public OrderDTO Order { get; set; }
+    public Guid ProductId { get; set; }
+    public ProductDTO Product { get; set; }
+    public int Quantity { get; set; }
+}
