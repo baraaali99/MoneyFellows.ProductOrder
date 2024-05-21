6b7467670d6305482acde77f116beac86b7ca5c8
baraaali99 <baraa.ali1912@gmail.com>
2024-05-21 17:08:46 +0300

    - starting implementing the controller - implemented GetProducts - implemented GetProductById

---
+++ MoneyFellows.ProductOrder.API/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MoneyFellows.ProductOrder.Application.DTOs;
+using MoneyFellows.ProductOrder.Application.IServices;
+using MoneyFellows.ProductOrder.Core.Models;
+
+namespace MoneyFellows.ProductOrder.API.Controllers;
+[ApiController]
+[Route("api/v1/products")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+    private readonly IMapper _mapper;
+
+    public ProductController(IProductService productService, IMapper mapper)
+    {
+        _productService = productService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult>GetProducts()
+    {
+        var products = _productService.GetAllProductsAsync();
+        var productsDto = _mapper.Map<IEnumerable<ProductDTO>>(products);
+        return Ok(productsDto);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductById(Guid id)
+    {
+        var product = await _productService.GetProductByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        var productDto = _mapper.Map<ProductDTO>(product);
+        return Ok(productDto);
+    }
+    
+}
