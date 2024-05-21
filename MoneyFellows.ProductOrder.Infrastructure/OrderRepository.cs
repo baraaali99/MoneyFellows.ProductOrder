a65a93138f825008692b239cfadcdc47f0872dfa
baraaali99 <baraa.ali1912@gmail.com>
2024-05-21 14:17:52 +0300

    - Implemented interface IOrderRepository in OrderRepository.cs

---
+++ MoneyFellows.ProductOrder.Infrastructure/OrderRepository.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using MoneyFellows.ProductOrder.Core.Interfaces;
+using MoneyFellows.ProductOrder.Core.Models;
+using MoneyFellows.ProductOrder.Infrastructure.Data;
+
+namespace MoneyFellows.ProductOrder.Infrastructure;
+
+public class OrderRepository : IOrderRepository
+{
+    public ProductOrderDbContext _DbContext;
+    public OrderRepository(ProductOrderDbContext _dbContext)
+    {
+        _DbContext = _dbContext;
+    }
+    public async Task<Order> GetByIdAsync(Guid id)
+    {
+        return await _DbContext.Orders.FirstOrDefaultAsync(o => o.Id == id);
+    }
+
+    public async Task<IEnumerable<Order>> GetAllAsync()
+    {
+        return await _DbContext.Orders.ToListAsync();
+    }
+
+    public async Task AddAsync(Order order)
+    {
+        await _DbContext.Orders.AddAsync(order);
+        await _DbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Order order)
+    {
+        _DbContext.Orders.Update(order);
+        await _DbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var order = await _DbContext.Orders.FirstOrDefaultAsync(o => o.Id == id);
+        if (order != null)
+        {
+            _DbContext.Orders.Remove(order);
+            await _DbContext.SaveChangesAsync();
+        }
+    }
+}
