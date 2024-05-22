0a03a60adb46b6dbf11a2b573484396a905d639d
baraaali99 <baraa.ali1912@gmail.com>
2024-05-23 02:29:39 +0300

    - Added Pagination for products

--- MoneyFellows.ProductOrder.Core/Interfaces/IProductRepository.cs
+++ MoneyFellows.ProductOrder.Core/Interfaces/IProductRepository.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using MoneyFellows.ProductOrder.Core.Models;
 
 namespace MoneyFellows.ProductOrder.Core.Interfaces;
@@ -8,7 +5,7 @@ namespace MoneyFellows.ProductOrder.Core.Interfaces;
 public interface IProductRepository
 {   
     Task<Product> GetByIdAsync(Guid id);
-    Task<IEnumerable<Product>> GetAllAsync();
+    Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize);
     Task AddAsync(Product product);
     Task UpdateAsync(Product product);
     Task DeleteAsync(Guid id);
