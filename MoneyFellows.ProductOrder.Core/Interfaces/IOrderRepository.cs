f42bb1ec1f669b0720e3e163f9359948a870a0a6
baraaali99 <baraa.ali1912@gmail.com>
2024-05-23 15:54:40 +0300

    - included OrderDetails do it can be shown in the response

--- MoneyFellows.ProductOrder.Core/Interfaces/IOrderRepository.cs
+++ MoneyFellows.ProductOrder.Core/Interfaces/IOrderRepository.cs
@@ -4,9 +4,9 @@ namespace MoneyFellows.ProductOrder.Core.Interfaces;
 
 public interface IOrderRepository
 {
-    Task<Order> GetByIdAsync(Guid id);
-    Task<IEnumerable<Order>> GetAllAsync(int pageNumber, int pageSize);
-    Task AddAsync(Order order);
-    Task UpdateAsync(Order order);
+    Task<Order?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Order?>> GetAllAsync(int pageNumber, int pageSize);
+    Task AddAsync(Order? order);
+    Task UpdateAsync(Order? order);
     Task DeleteAsync(Guid id);
 }
