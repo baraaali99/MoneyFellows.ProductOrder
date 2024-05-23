defdfc2892e0eceae49f22fc2e8db6dbfd209e47
baraaali99 <baraa.ali1912@gmail.com>
2024-05-23 03:01:19 +0300

    - Added some mapping profiles for Product commands and queries

---
+++ MoneyFellows.ProductOrder.Application/Products/MappingProfile/ProductMappingProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using MoneyFellows.ProductOrder.Application.DTOs;
+using MoneyFellows.ProductOrder.Application.Products.Commands;
+using MoneyFellows.ProductOrder.Application.Products.Dtos;
+using MoneyFellows.ProductOrder.Core.Models;
+
+
+namespace MoneyFellows.ProductOrder.Application.Products.MappingProfile
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<CreateProductCommand, Product>();
+            CreateMap<Product, GetProductsListQueryOutputDtoItem>().ReverseMap();
+            CreateMap<Product, GetProductByIdQueryDto>();
+            CreateMap<UpdateProductCommand, Product>();
+        }
+    }
+}
