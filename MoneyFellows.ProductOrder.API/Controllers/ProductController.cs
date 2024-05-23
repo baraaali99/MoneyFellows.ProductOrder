2b7a5966d61d44b826b9167844299e65106476aa
baraaali99 <baraa.ali1912@gmail.com>
2024-05-23 03:04:44 +0300

    - Added Crud Endpoints

--- MoneyFellows.ProductOrder.API/Controllers/ProductController.cs
+++ MoneyFellows.ProductOrder.API/Controllers/ProductController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using MoneyFellows.ProductOrder.Application.DTOs;
 using MoneyFellows.ProductOrder.Application.IServices;
+using MoneyFellows.ProductOrder.Application.Products.Commands;
+using MoneyFellows.ProductOrder.Application.Products.Queries;
 using MoneyFellows.ProductOrder.Core.Models;
 
 namespace MoneyFellows.ProductOrder.API.Controllers;
@@ -12,33 +14,54 @@ namespace MoneyFellows.ProductOrder.API.Controllers;
 [Route("api/v1/products")]
 public class ProductController : ControllerBase
 {
-    private readonly IProductService _productService;
-    private readonly IMapper _mapper;
-
-    public ProductController(IProductService productService, IMapper mapper)
+    private readonly IMediator _mediator;
+    public ProductController(IMediator mediator)
     {
-        _productService = productService;
-        _mapper = mapper;
+        _mediator = mediator;
     }
 
     [HttpGet]
-    public async Task<IActionResult>GetProducts()
+    public async Task<IActionResult>GetProducts([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 20)
     {
-        var products = await _productService.GetAllProductsAsync();
-        var productsDto = _mapper.Map<IEnumerable<ProductDTO>>(products);
-        return Ok(productsDto);
+        var getProductListQuery = new GetProductsListQuery
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+        var products = await _mediator.Send(getProductListQuery);
+        return Ok(products);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] CreateProductCommand createProductCommand)
+    {
+        await _mediator.Send(createProductCommand);
+        return Ok();
     }
     
     [HttpGet("{id}")]
     public async Task<IActionResult> GetProductById(Guid id)
     {
-        var product = await _productService.GetProductByIdAsync(id);
-        if (product == null)
-        {
-            return NotFound();
-        }
-        var productDto = _mapper.Map<ProductDTO>(product);
-        return Ok(productDto);
+        var product = await _mediator.Send(new GetProductByIdQuery(id));
+        return Ok(product);
     }
-    
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(Guid id, [FromBody] UpdateProductCommand updateProductCommand)
+    {
+        updateProductCommand.Id = id;
+        await _mediator.Send(updateProductCommand);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(Guid id)
+    {
+        var deleteProductCommand = new DeleteProductCommand()
+        {
+            Id = id
+        };
+        await _mediator.Send(deleteProductCommand);
+        return Ok();
+    }
 }
