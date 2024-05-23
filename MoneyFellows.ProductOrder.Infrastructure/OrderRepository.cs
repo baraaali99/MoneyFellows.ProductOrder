0a0cf0bd0ce40c452ba82f835807928d047180c6
baraaali99 <baraa.ali1912@gmail.com>
2024-05-23 16:36:24 +0300

    - included OrderDetails so it can appear in the response

--- MoneyFellows.ProductOrder.Infrastructure/OrderRepository.cs
+++ MoneyFellows.ProductOrder.Infrastructure/OrderRepository.cs
@@ -12,23 +12,25 @@ public class OrderRepository : IOrderRepository
     {
         _DbContext = _dbContext;
     }
-    public async Task<Order> GetByIdAsync(Guid id)
+    public async Task<Order?> GetByIdAsync(Guid id)
     {
-        return await _DbContext.Orders.FirstOrDefaultAsync(o => o.Id == id);
+        return await _DbContext.Orders.Include(o => o.OrderDetails)
+            .FirstOrDefaultAsync(o => o.Id == id);
     }
 
-    public async Task<IEnumerable<Order>> GetAllAsync(int pageNumber, int pageSize)
+    public async Task<IEnumerable<Order?>> GetAllAsync(int pageNumber, int pageSize)
     {
-        return await _DbContext.Orders.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return await _DbContext.Orders.Include(o => o.OrderDetails)
+            .Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
     }
 
-    public async Task AddAsync(Order order)
+    public async Task AddAsync(Order? order)
     {
         await _DbContext.Orders.AddAsync(order);
         await _DbContext.SaveChangesAsync();
     }
 
-    public async Task UpdateAsync(Order order)
+    public async Task UpdateAsync(Order? order)
     {
         _DbContext.Orders.Update(order);
         await _DbContext.SaveChangesAsync();
