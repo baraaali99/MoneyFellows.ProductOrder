ed82c37fe4045d543155065dd38137639c06e823
baraaali99 <baraa.ali1912@gmail.com>
2024-05-23 03:00:39 +0300

    - did some cleaning

--- MoneyFellows.ProductOrder.Application/Products/Queries/GetProductByIdQuery.cs
+++ MoneyFellows.ProductOrder.Application/Products/Queries/GetProductByIdQuery.cs
@@ -1,11 +1,11 @@
 using AutoMapper;
 using MediatR;
-using MoneyFellows.ProductOrder.Application.DTOs;
+using MoneyFellows.ProductOrder.Application.Products.Dtos;
 using MoneyFellows.ProductOrder.Core.Interfaces;
 
 namespace MoneyFellows.ProductOrder.Application.Products.Queries;
 
-public class GetProductByIdQuery : IRequest<ProductDTO>
+public class GetProductByIdQuery : IRequest<GetProductByIdQueryDto>
 {
     public Guid Id { get;}
 
@@ -15,7 +15,7 @@ public class GetProductByIdQuery : IRequest<ProductDTO>
     }
 }
 
-public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductDTO>
+public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, GetProductByIdQueryDto>
 {
     private readonly IProductRepository _productRepository;
     private readonly IMapper _mapper;
@@ -25,10 +25,10 @@ public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, P
         _productRepository = productRepository;
         _mapper = mapper;
     }
-    public async Task<ProductDTO> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    public async Task<GetProductByIdQueryDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
     {
         var product = await _productRepository.GetByIdAsync(request.Id);
-        var productDto = _mapper.Map<ProductDTO>(product);
+        var productDto = _mapper.Map<GetProductByIdQueryDto>(product);
         return productDto;
     }
 }
