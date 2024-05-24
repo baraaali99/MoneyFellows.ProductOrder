53361c52be0d82243164146e302837513288c1e1
baraaali99 <baraa.ali1912@gmail.com>
2024-05-24 20:11:16 +0300

    - Created command for Update order

---
+++ MoneyFellows.ProductOrder.Application/Orders/Commands/UpdateOrderCommand.cs
@@ -0,0 +1,48 @@
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using MoneyFellows.ProductOrder.Core.Interfaces;
+using MoneyFellows.ProductOrder.Core.Models;
+
+namespace MoneyFellows.ProductOrder.Application.Orders.Commands;
+
+public class UpdateOrderCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string DeliveryAddress { get; set; }
+    public decimal TotalCost { get; set; }
+    public List<OrderDetails> OrderDetails { get; set; }
+    public string CustomerDetails { get; set; }
+    public DateTime DeliveryTime { get; set; }
+}
+
+public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+    {
+        _orderRepository = orderRepository;
+        _mapper = mapper;
+    }
+    public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(request.Id);
+        if (order == null)
+        {
+            throw new Exception("order doesn't exist");
+        }
+        ValidateDeliveryTime(order, request);
+        _mapper.Map(request, order);
+        await _orderRepository.UpdateAsync(order);
+        return Unit.Value;
+    }
+    private void ValidateDeliveryTime(Order originalOrder, UpdateOrderCommand updateOrderCommand)
+    {
+        if (updateOrderCommand.DeliveryTime < originalOrder.DeliveryTime)
+        {
+            throw new ValidationException("Delivery Time cannot be set to a time earlier than the original delivery time.");
+        }
+    }
+}
