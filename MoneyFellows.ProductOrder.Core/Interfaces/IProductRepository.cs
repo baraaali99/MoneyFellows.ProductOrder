18b0f4d03937cdaea53e9dea6821c1692991ac67
baraaali99 <baraa.ali1912@gmail.com>
2024-05-24 18:11:07 +0300

    - made return type in getById can be nullable

--- MoneyFellows.ProductOrder.Core/Interfaces/IProductRepository.cs
+++ MoneyFellows.ProductOrder.Core/Interfaces/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace MoneyFellows.ProductOrder.Core.Interfaces;
 
 public interface IProductRepository
 {   
-    Task<Product> GetByIdAsync(Guid id);
+    Task<Product?> GetByIdAsync(Guid id);
     Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize,Expression<Func<Product, bool>>? filter = null, 
         Func<IQueryable<Product>, IOrderedQueryable<Product>>? orderBy = null);
     Task AddAsync(Product product);
