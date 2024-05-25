81ab6350f224a8bbf61d7abca8581c3a6920cf51
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 16:47:42 +0300

    - Added validator for delete command

---
+++ MoneyFellows.ProductOrder.Application/Products/Commands/DeleteProductCommand/DeleteProductCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace MoneyFellows.ProductOrder.Application.Products.Commands.DeleteProductCommand
+{
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Product Id is required.")
+                .GreaterThan(0).WithMessage("Product Id must be greater than zero.");
+        }
+    }
+}
