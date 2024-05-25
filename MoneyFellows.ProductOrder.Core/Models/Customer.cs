18ef8a838d2af7e257f4529250be69400840a4b2
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 15:15:03 +0300

    - changed class name and changed Guid to int

---
+++ MoneyFellows.ProductOrder.Core/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace MoneyFellows.ProductOrder.Core.Models;
+
+public class Customer
+{
+    public int Id { set; get; }
+    public string Name { set; get; }
+    public string Email { get; set; }
+    public string ContactNumber { get; set; }
+}
