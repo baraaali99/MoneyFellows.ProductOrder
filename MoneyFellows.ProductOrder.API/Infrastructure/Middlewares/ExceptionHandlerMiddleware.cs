816af48e087aa480680aaed389d12b0117e3af28
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 15:15:25 +0300

    added an exception handler middleware

---
+++ MoneyFellows.ProductOrder.API/Infrastructure/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+
+namespace MoneyFellows.ProductOrder.API.Infrastructure.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(
+            RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private static void HandleException(Exception exception)
+        {
+            Log.Error(exception, "An error Occured");
+        }
+    }
+}
