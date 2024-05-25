7b57dc166119558d35a9827b164fbd04ba11bafe
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 16:58:21 +0300

    - added id field

--- MoneyFellows.ProductOrder.Application/Products/Dtos/GetProductsListQueryOutputDto.cs
+++ MoneyFellows.ProductOrder.Application/Products/Dtos/GetProductsListQueryOutputDto.cs
@@ -9,6 +9,7 @@ public class GetProductsListQueryOutputDto
 
 public class GetProductsListQueryOutputDtoItem
 {
+    public int Id { get; set; }
     public string ProductName { get; set; } = string.Empty;
     public string ProductDescription { get; set; } = string.Empty;
     public byte[]? ProductImage { get; set; }
