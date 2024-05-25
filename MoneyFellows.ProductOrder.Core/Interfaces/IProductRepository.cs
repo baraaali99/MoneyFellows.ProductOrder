7f8824d8df495bea0b8a4aab44e05ab2e154c3cc
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 17:02:38 +0300

    - Added field that will help in filter, implemented two new functions - IsProductExistsAsync is for unique validation - IsProductOrderedBeforeAsync used in validating that Products included in existing orders can’t be deleted.

--- MoneyFellows.ProductOrder.Core/Interfaces/IProductRepository.cs
+++ MoneyFellows.ProductOrder.Core/Interfaces/IProductRepository.cs
@@ -6,8 +6,12 @@ namespace MoneyFellows.ProductOrder.Core.Interfaces;
 public interface IProductRepository
 {   
     Task<Product?> GetByIdAsync(int id);
-    Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize);
+    Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize,
+        decimal? minPrice, decimal? maxPrice, string? merchant,
+        string sortBy, bool sortAscending, string? searchTerm);
     Task AddAsync(Product product);
     Task UpdateAsync(Product product);
     Task DeleteAsync(int id);
+    Task<bool> IsProductExistsAsync(string productName);
+    Task<bool> IsProductOrderedBeforeAsync(int productId);
 }
