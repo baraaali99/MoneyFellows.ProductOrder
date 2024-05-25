85b9727f9fbb9b10f59373167bed8224773d9082
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 19:03:17 +0300

    - fixed incorrect mappings, and added more mappings for commands and queries

--- MoneyFellows.ProductOrder.Application/Orders/MappingProfile/OrderMappingProfile.cs
+++ MoneyFellows.ProductOrder.Application/Orders/MappingProfile/OrderMappingProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
-using MoneyFellows.ProductOrder.Application.Orders.Commands;
+using MoneyFellows.ProductOrder.Application.Orders.Commands.CreateOrderCommand;
+using MoneyFellows.ProductOrder.Application.Orders.Commands.UpdateOrderCommand;
 using MoneyFellows.ProductOrder.Application.Orders.Dtos;
 using MoneyFellows.ProductOrder.Core.Models;
 using OrderDetail = MoneyFellows.ProductOrder.Core.Models.OrderDetail;
@@ -10,11 +11,19 @@ public class OrderMappingProfile : Profile
 {
     public OrderMappingProfile()
     {
-        CreateMap<CreateOrderCommand, Product>();
+        CreateMap<CreateOrderCommand, Order>();
+        CreateMap<CreateOrderCommandCustomer, Customer>();
+        CreateMap<CreateOrderCommandOrderDetail, OrderDetail>();
+
+        CreateMap<UpdateOrderCommand, Order>();
+        CreateMap<UpdateOrderCommandCustomer, Customer>();
+        CreateMap<UpdateOrderCommandOrderDetail, OrderDetail>();
+
         CreateMap<Order, GetOrdersListQueryOutputDtoItem>()
             .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails));
         CreateMap<OrderDetail, GetOrderDetailsDto>();
         CreateMap<Order, GetOrderbyIdQueryDto>();
-
+        CreateMap<OrderDetail, OrderDetailByIdDto>();
+        CreateMap<Customer, CustomerDetailsDto>();
     }
 }
