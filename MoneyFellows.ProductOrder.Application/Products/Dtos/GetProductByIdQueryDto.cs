e24cbe5277de98773d18fc5d3e4614fce9421ee8
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 16:55:22 +0300

    - modified the class by adding Id field, and fixed a bug by changing private members to public

--- MoneyFellows.ProductOrder.Application/Products/Dtos/GetProductByIdQueryDto.cs
+++ MoneyFellows.ProductOrder.Application/Products/Dtos/GetProductByIdQueryDto.cs
@@ -2,9 +2,10 @@ namespace MoneyFellows.ProductOrder.Application.Products.Dtos;
 
 public class GetProductByIdQueryDto
 {
-    private string ProductName { get; set; } = string.Empty;
-    private string ProductDescription { get; set; } = string.Empty;
-    private byte[]? ProductImage { get; set; }
-    private decimal Price { get; set; }
-    private string Merchant { get; set; } = string.Empty;
+    public int Id { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string ProductDescription { get; set; } = string.Empty;
+    public byte[]? ProductImage { get; set; }
+    public decimal Price { get; set; }
+    public string Merchant { get; set; } = string.Empty;
 }
