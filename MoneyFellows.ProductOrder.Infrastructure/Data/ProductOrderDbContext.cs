b79cd9b1af56b8f4fa18d3b5d73123e65605c56f
baraaali99 <baraa.ali1912@gmail.com>
2024-05-25 15:13:23 +0300

    - fixed naming convention and removed unnecessary configuration

--- MoneyFellows.ProductOrder.Infrastructure/Data/ProductOrderDbContext.cs
+++ MoneyFellows.ProductOrder.Infrastructure/Data/ProductOrderDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using MoneyFellows.ProductOrder.Core.Models;
 
 namespace MoneyFellows.ProductOrder.Infrastructure.Data;
@@ -15,26 +14,9 @@ public class ProductOrderDbContext : DbContext
     {
         // for scaffolding
     }
-    public  DbSet<Product> Products { get; set; }
-    public  DbSet<Order?> Orders { get; set; }
-    public DbSet<OrderDetails> orderDetails { get; set; }
-    public DbSet<CustomerDetails> CustomerDetailsEnumerable { get; set; }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        // Product configuration
-        modelBuilder.Entity<Product>()
-            .HasIndex(p => new { p.ProductName })
-            .IsUnique(true);
-        
-        modelBuilder.Entity<OrderDetails>()
-            .HasOne(od => od.Product)
-            .WithMany()
-            .HasForeignKey(od => od.ProductId)
-            .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<OrderDetails>()
-            .HasIndex(od => new { od.ProductId, od.OrderId })
-            .IsUnique(true);
-    }
-    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 }
